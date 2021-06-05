59f2186ecbcf4d8db2bc9287e1a7c70c8c405313
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-06 01:55:14 +0300

    added logger

---
+++ logger/GlobalLogger.py
@@ -0,0 +1,28 @@
+import enum
+import time
+
+
+class EventType(enum.Enum):
+    ERROR = enum.auto(),
+    INFO = enum.auto(),
+    WARNING = enum.auto(),
+
+
+class GlobalLogger:
+    @staticmethod
+    def log(event_type: EventType, msg: str):
+        result_msg = '{0:10}:{1:15} {2}\n'.format(event_type, time.ctime(time.time()), msg)
+
+        print(result_msg)
+
+    @staticmethod
+    def log_warning(msg):
+        GlobalLogger.log(EventType.WARNING, msg)
+
+    @staticmethod
+    def log_error(msg):
+        GlobalLogger.log(EventType.ERROR, msg)
+
+    @staticmethod
+    def log_info(msg):
+        GlobalLogger.log(EventType.INFO, msg)
