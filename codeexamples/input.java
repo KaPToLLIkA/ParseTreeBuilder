4ba2a91e39622bdff1997fc6d8b9f196437a6563
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-06 21:32:22 +0300

    added code example

---
+++ codeexamples/input.java
@@ -0,0 +1,55 @@
+
+
+class Main {
+  public static final int t = 2;
+  private float x = 3.5;
+  protected static final char c = '1';
+
+  public static void t(int a, int b, int c) {
+    v = a + b - c;
+  }
+
+  public class Name {
+    public static final char n = 'N';
+
+  }
+
+  public static void main(String args[]) {
+    int a = 3;
+
+    {
+        int a = 13;
+        int b = 14;
+        int a[];
+        int b[] = new int [5] { a, 3, a, 10 };
+
+        for (; a < 14;) {
+            if (b == 13) {
+                do { a = 0; } while (a > 3);
+            }
+            else if (b == 10) {}
+            else {
+                if (b == 1)
+                    b = 2;
+            }
+        }
+    }
+
+    {
+        char t = 'e';
+        {
+            char a = 'a';
+            int v;
+            v = a + (b - a) / 1 - ((a - 3) * 3) - 4;
+            {
+                {
+                    {
+                        char e = 'e';
+                    }
+                }
+            }
+        }
+    }
+
+  }
+}
