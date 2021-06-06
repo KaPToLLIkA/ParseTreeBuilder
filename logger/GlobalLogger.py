5d312a69437e7fce5fc98a9472a7432151dba0fa
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-06 13:07:27 +0300

    update logger, added on/off

--- logger/GlobalLogger.py
+++ logger/GlobalLogger.py
@@ -10,13 +10,30 @@ class EventType(enum.Enum):
 
 class GlobalLogger:
     enabled = True
+    console_log_enabled = True
+
+    @staticmethod
+    def on():
+        GlobalLogger.enabled = True
+
+    @staticmethod
+    def off():
+        GlobalLogger.enabled = False
 
     @staticmethod
     def log(event_type: EventType, msg: str):
         if GlobalLogger.enabled:
-            result_msg = '{0:10}:{1:15} {2}\n'.format(event_type, time.ctime(time.time()), msg)
+            time_str = time.strftime('%H:%M:%S', time.localtime())
+            max_time_str_len = len(time_str)
+            max_event_type_str_len = 0
+            for name, member in EventType.__members__.items():
+                max_event_type_str_len = max(max_event_type_str_len, len(name))
 
-            print(result_msg)
+            pre_result_msg = '{0:%d} ## {1:%d} ## {2}' % (max_event_type_str_len, max_time_str_len)
+            result_msg = pre_result_msg.format(event_type.name, time_str, msg)
+
+            if GlobalLogger.console_log_enabled:
+                print(result_msg)
 
     @staticmethod
     def log_warning(msg):
