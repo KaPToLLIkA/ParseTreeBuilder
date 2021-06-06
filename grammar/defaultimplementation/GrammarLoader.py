da0050e78685e27bee1bd91204a5ec2d285d85ef
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-06 13:53:48 +0300

    add black list of characters

--- grammar/defaultimplementation/GrammarLoader.py
+++ grammar/defaultimplementation/GrammarLoader.py
@@ -3,6 +3,9 @@ import codecs
 from grammar.interfaces.IGrammarLoader import IGrammarLoader
 
 
+characters_black_list = '\n\t\b\r\f\v\a'
+
+
 class GrammarLoader(IGrammarLoader):
     def __init__(self):
         self.raw_grammar = ""
@@ -12,7 +15,11 @@ class GrammarLoader(IGrammarLoader):
         in_file = codecs.open(file_name, 'r', 'utf-8')
 
         for line in in_file:
-            _line = str(line).replace('\n', '').replace('\r', '')
+            _line = line
+
+            for character in characters_black_list:
+                _line = str(_line).replace(character, '')
+
             if _line and _line[0] != '#':
                 self.raw_grammar += _line
 
