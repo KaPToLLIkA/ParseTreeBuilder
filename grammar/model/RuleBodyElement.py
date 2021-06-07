7b214ec60f3fda7495981a70f7ba9e5f9cf3ba00
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-07 20:42:16 +0300

    add __str__ method in RuleBodyElement.py

--- grammar/model/RuleBodyElement.py
+++ grammar/model/RuleBodyElement.py
@@ -11,3 +11,6 @@ class RuleBodyElement:
     def __init__(self, string: str, element_type: RuleBodyElementType):
         self.element_type = element_type
         self.string = string
+
+    def __str__(self):
+        return f'Element: {self.element_type.name:10} {self.string}'
