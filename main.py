72645f3d000b6f4bd10069ba8eaf6e56bea21622
purdenko_konstantine <eduardpur18@gmail.com>
2021-06-09 02:49:49 +0300

    disable grammar analyzer

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ file_with_grammar = 'java_context_free_grammar.grm'
 file_with_code = './codeexamples/input.java'
 
 if __name__ == '__main__':
-    grammar_loader = GrammarLoader()
-    grammar_loader.load_from_file(file_with_grammar)
-
-    grammar_analyzer = GrammarAnalyzer()
-    grammar_analyzer.analyze(grammar_loader.raw_grammar)
-
-    GlobalLogger.log_info('Grammar description:')
-    GlobalLogger.log_info(grammar_analyzer.grammar)
-    GlobalLogger.log_info('End of grammar description.')
+    # grammar_loader = GrammarLoader()
+    # grammar_loader.load_from_file(file_with_grammar)
+    #
+    # grammar_analyzer = GrammarAnalyzer()
+    # grammar_analyzer.analyze(grammar_loader.raw_grammar)
+    #
+    # GlobalLogger.log_info('Grammar description:')
+    # GlobalLogger.log_info(grammar_analyzer.grammar)
+    # GlobalLogger.log_info('End of grammar description.')
 
     code_loader = CodeLoader(file_with_code)
     code_loader.load()
